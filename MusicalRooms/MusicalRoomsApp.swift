20e75adfc58f772ecde1cd1f1a1d420bd762bd7c
Xuyuan <t.xuyuan@gmail.com>
2021-11-14 19:38:27 +0800

    Initial Commit

---
+++ MusicalRooms/MusicalRoomsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicalRoomsApp.swift
+//  MusicalRooms
+//
+//  Created by sap on 14/11/21.
+//
+
+import SwiftUI
+
+@main
+struct MusicalRoomsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

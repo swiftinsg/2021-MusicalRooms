5c49d5455a321a91ad2adf6c24ef610f10fab4e7
chlxi <chelsealx2208@gmail.com>
2021-11-15 11:02:30 +0800

    added standard colour sets and a bit of the sight-reading screen

--- MusicalRooms/SightreadingRoom/ReadingHomeView.swift
+++ MusicalRooms/SightreadingRoom/ReadingHomeView.swift
@@ -10,6 +10,25 @@ import SwiftUI
 struct ReadingHomeView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(alignment: .bottom) {
+            VStack(alignment: .leading) {
+                Text("Alto")
+                    .fontWeight(.semibold)
+                    .multilineTextAlignment(.leading)
+                    .font(.system(.title, design: .rounded))
+                    .lineLimit(3)
+                    .foregroundColor(Color("darkBrown"))
+                
+                Spacer()
+            }
+            
+            Spacer()
+            
+        }
+        .padding()
+        .frame(width:380, height: 170, alignment: .leading)
+        .background( Color("lightBrown"))
+        .cornerRadius(20)
     }
 }
 

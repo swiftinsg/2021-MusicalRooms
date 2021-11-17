251a3cdaf5c66cbf5363a3e8db49f0981692ed77
isaac <baa44422@gmail.com>
2021-11-17 13:08:43 +0800

    (For the shapes) made it so that only one button would be selected

--- MusicalRooms/TunerRoom/TunerHomeView.swift
+++ MusicalRooms/TunerRoom/TunerHomeView.swift
@@ -1,37 +1,62 @@
 import SwiftUI
 
-struct SharpTextViewModifier: ViewModifier {
-    func body(content: Content) -> some View {
-        content
-            .font(.system(size: 20, design: .rounded))
-            .foregroundColor(Color("darkBrown"))
-    }
+struct Note: Identifiable, Equatable {
+    var id = UUID()
+    
+    var name: String
+    var isSelected: Bool = false
 }
 
 struct TunerHomeView: View {
     
-    @State var CtoB = ["C", "D", "E", "F", "G", "A", "B"]
-    @State var sharps = ["C#", "D#", "F#", "G#", "A#"]
-    
-    @State var isSoundPlayed = false
+    @State var notes = [
+        Note(name: "C"),
+        Note(name: "D"),
+        Note(name: "E"),
+        Note(name: "F"),
+        Note(name: "G"),
+        Note(name: "A"),
+        Note(name: "B"),
+        Note(name: "C#"),
+        Note(name: "D#"),
+        Note(name: "F#"),
+        Note(name: "G#"),
+        Note(name: "A#")]
+
     var body: some View {
         VStack {
             HStack {
-                ForEach(0 ..< sharps.count) { note in
+                ForEach(7 ..< notes.count) { note in
                     Button {
-                        
+                        for num in 0 ..< notes.count {
+                            notes[num].isSelected = false
+                            print(index)
+                        }
+                        notes[note].isSelected.toggle()
                     } label: {
                         VStack {
-                            Text(sharps[note])
-                                .brownBoldText()
-                            Text(getEnharmonic(sharps[note]))
-                                .brownBoldText()
+                            Text(notes[note].name)
+                                .fontWeight(.semibold)
+                            Text(getEnharmonic(notes[note].name))
+                                .fontWeight(.semibold)
                         }
+                        .font(.system(size: 20, design: .rounded))
+                        .multilineTextAlignment(.center)
+                        .foregroundColor(!notes[note].isSelected ? Color("darkBrown") : Color("evenLighterBrown"))
                     }
                     .padding()
                     .frame(width:60, height: 100, alignment: .leading)
-                    .background( Color("evenLighterBrown"))
+                    .background(!notes[note].isSelected ? Color("evenLighterBrown") : Color("darkBrown"))
                     .cornerRadius(20)
+                }
+            }
+            HStack {
+                ForEach(7 ..< notes.count) { note in
+                    Button {
+                        
+                    } label: {
+                        Text(notes[note].name)
+                    }
                 }
             }
         }

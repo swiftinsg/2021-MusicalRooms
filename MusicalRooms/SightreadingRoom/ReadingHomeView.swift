b19a1bf7c750e4c0918f13dbee079ef64ca0be5a
isaac <baa44422@gmail.com>
2021-11-17 11:08:30 +0800

    Changing the texts with tap gestures to navigation links so that we can make a function to determine which view to go to

--- MusicalRooms/SightreadingRoom/ReadingHomeView.swift
+++ MusicalRooms/SightreadingRoom/ReadingHomeView.swift
@@ -8,20 +8,20 @@
 import SwiftUI
 
 struct ReadingHomeView: View {
-
+    
     @State var clefs = ["Treble", "Alto", "Tenor", "Bass"]
     @State var timedQuestions = [15,30,45,60]
     @State var drilledQuestions = [10,20,30,40]
-
+    
     var body: some View {
-
+        
         //Header
         VStack(alignment: .leading) {
             Text("Sightreading")
                 .font(Font.system(size: 32, weight: .bold))
                 .frame( alignment: .leading)
                 .padding()
-
+            
             ScrollView(.vertical, showsIndicators: false) {
                 ForEach(0 ..< clefs.count) { clef in
                     HStack(alignment: .center) {
@@ -33,18 +33,19 @@ struct ReadingHomeView: View {
                                 .font(.system(.title, design: .rounded))
                                 .lineLimit(3)
                                 .foregroundColor(Color("darkBrown"))
-
+                            
                             VStack(alignment: .leading) {
                                 HStack {
                                     Text("Timed:")
                                         .frame(width: 60, height: 20)
                                     HStack {
                                         ForEach(0 ..< timedQuestions.count) { time in
-                                            Text("\(timedQuestions[time])s")
-                                                .frame(width: 40, height: 20, alignment: .center)
-                                                .onTapGesture {
-                                                    //navigation link to be addded to SightreadingQuizView
-                                                }
+                                            NavigationLink {
+                                                
+                                            } label: {
+                                                Text("\(timedQuestions[time])s")
+                                                    .frame(width: 40, height: 20, alignment: .center)
+                                            }
                                         }
                                     }
                                 }
@@ -52,17 +53,18 @@ struct ReadingHomeView: View {
                                 .font(.system(size: 20,design: .rounded))
                                 .lineLimit(3)
                                 .foregroundColor(Color("darkerBrown"))
-
+                                
                                 HStack {
                                     Text("Drill:")
                                         .frame(width: 60, height: 20, alignment: .leading)
                                     HStack {
                                         ForEach(0 ..< drilledQuestions.count) { drill in
-                                            Text("\(drilledQuestions[drill])")
-                                                .frame(width: 40, height: 20, alignment: .center)
-                                                .onTapGesture {
-
-                                                }
+                                            NavigationLink {
+                                                
+                                            } label: {
+                                                Text("\(drilledQuestions[drill])")
+                                                    .frame(width: 40, height: 20, alignment: .center)
+                                            }
                                         }
                                     }
                                 }
@@ -75,7 +77,7 @@ struct ReadingHomeView: View {
                             .frame(width: 280, height: 80, alignment: .leading)
                             .background( Color("evenLighterBrown"))
                             .cornerRadius(20)
-
+                            
                         }
                     }
                     .padding()

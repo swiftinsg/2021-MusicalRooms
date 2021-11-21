d3d1156b7ee927663711f53604428769c188a4f4
isaac <baa44422@gmail.com>
2021-11-21 21:14:05 +0800

    Creating a new view for the user to see what the meanings of the words are

---
+++ MusicalRooms/TheoryRoom/DefinitionModalView.swift
@@ -0,0 +1,20 @@
+//
+//  DefinitionModalView.swift
+//  MusicalRooms
+//
+//  Created by Isaac Lee Jing Zhi on 21/11/21.
+//
+
+import SwiftUI
+
+struct DefinitionModalView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DefinitionModalView_Previews: PreviewProvider {
+    static var previews: some View {
+        DefinitionModalView()
+    }
+}

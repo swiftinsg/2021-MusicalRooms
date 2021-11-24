f3f460cfad013b4bf919dd22005be1d300c07495
isaac <baa44422@gmail.com>
2021-11-24 22:33:45 +0800

    More UI fixes and added features

--- MusicalRooms/TheoryRoom/TheoryHomeView.swift
+++ MusicalRooms/TheoryRoom/TheoryHomeView.swift
@@ -13,12 +13,12 @@ struct TheoryHomeView: View {
     @State var percentCorrect: Double = 50
     
     var body: some View {
-        VStack {
-            Text("Theory")
+        NavigationView {
+            VStack {
+                Text("Theory")
                     .font(Font.system(size: 28, weight: .bold))
                     .padding()
-
-            ScrollView {
+                
                 ForEach(grades) { grade in
                     NavigationLink {
                         OverviewView(grade: grade.number )
@@ -26,41 +26,40 @@ struct TheoryHomeView: View {
                         VStack(alignment: .leading) {
                             HStack(alignment: .bottom) {
                                 Text("Grade \(grade.number)")
-                                        .multilineTextAlignment(.leading)
-                                        .font(Font.system(size: 25, weight: .semibold, design: .rounded))
-                                        .lineLimit(3)
-                                        .foregroundColor(Color("darkerBrown"))
-
+                                    .multilineTextAlignment(.leading)
+                                    .font(Font.system(size: 25, weight: .semibold, design: .rounded))
+                                    .lineLimit(3)
+                                    .foregroundColor(Color("darkerBrown"))
+                                
                                 Text(String(format: "%.2f", grade.percentageCorrect) + "%")
-                                        .multilineTextAlignment(.leading)
-                                        .font(Font.system(size: 25, weight: .semibold, design: .rounded))
-                                        .lineLimit(3)
-                                        .foregroundColor(Color("darkerBrown"))
-                                        .offset(x: 100)
-
+                                    .multilineTextAlignment(.leading)
+                                    .font(Font.system(size: 25, weight: .semibold, design: .rounded))
+                                    .lineLimit(3)
+                                    .foregroundColor(Color("darkerBrown"))
+                                    .offset(x: 100)
+                                
                             }.offset(y: -20)
-
+                            
                             //PROGRESS BAR
                             RoundedRectangle(cornerRadius: 5)
-                                    .foregroundColor(Color("darkLightBrown"))
-                                    .frame(width: 300, height: 10, alignment: .leading)
-                                    .overlay(
-                                        VStack(alignment: .leading) {
-                                            RoundedRectangle(cornerRadius: 5)
-                                                    .fill(Color("darkBrown"))
-                                                .frame(width: (grade.percentageCorrect / 100 * 300))
-                                        }
-                                    )
+                                .foregroundColor(Color("darkLightBrown"))
+                                .frame(width: 300, height: 10, alignment: .leading)
+                                .overlay(
+                                    VStack(alignment: .leading) {
+                                        RoundedRectangle(cornerRadius: 5)
+                                            .fill(Color("darkBrown"))
+                                            .frame(width: (grade.percentageCorrect / 100 * 300))
+                                    }
+                                )
                         }
                         .padding()
                         .frame(width: 330, height: 135, alignment: .leading)
                         .background(Color("lightBrown"))
                         .cornerRadius(10)
-
+                        
                     }.frame(width: 400)//END LABEL
                 }
             }.padding(.bottom, 22)
-
         }.offset(y:20)
     }
 }
